21e03615563952475261128fffd30d2eca2becf2
Gus Bikos <gusbikos@gmail.com>
2021-01-16 23:17:16 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,11 +5,10 @@ def find_min_in_nested_arrays(src)
   low_temp = []
   row_index = 0
 
-  while row_index < src.count do
-    lowest_temp = []
-    lowest_temp = src[row_index].min
+  while row_index < src.length do
+    lowest_temp = src[row_index]
+    low_temp = lowest_temp.min
     row_index += 1
   end
-
-  low_temp << lowest_temp
+  low_temp
 end
